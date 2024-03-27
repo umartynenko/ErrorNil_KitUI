68c306c18bd45a4a7d11b7791450e8e81c8d483d
Martynenko Uriy <uriy.martynenko@gmail.com>
2024-03-27 22:16:07 +0300

    Initial Commit

---
+++ ErrorNil_KitUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ErrorNil_KitUI
+//
+//  Created by Юрий Мартыненко on 27.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
