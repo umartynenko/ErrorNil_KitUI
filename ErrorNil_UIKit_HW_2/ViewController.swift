68f4f1caa4bc6dca1c3ce603c1c8345e3a3c6c27
Martynenko Uriy <uriy.martynenko@gmail.com>
2024-04-02 02:23:28 +0300

    HW_2

---
+++ ErrorNil_UIKit_HW_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ErrorNil_UIKit_HW_2
+//
+//  Created by Юрий Мартыненко on 01.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
