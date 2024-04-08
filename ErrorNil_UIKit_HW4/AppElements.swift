a71cdebb5d7f15d80c84c8a59ae89a14ec7316a6
Martynenko Uriy <uriy.martynenko@gmail.com>
2024-04-08 09:00:30 +0300

    Update

--- ErrorNil_UIKit_HW4/AppElements.swift
+++ ErrorNil_UIKit_HW4/AppElements.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import UIKit
+
+
+class AppElements {
+    static func createLabel(text: String, font: UIFont, size: CGSize, position: CGPoint, colot: UIColor) -> UILabel {
+        {
+            $0.text = text
+            $0.font = font
+            $0.frame.size = size
+            $0.frame.origin = position
+            $0.textColor = colot
+            
+            return $0
+        }(UILabel())
+    }
+    
+    static func createRoundButton(
+        color: UIColor = .clear,
+        logotip: String,
+        corner: CGFloat,
+        size: CGSize,
+        position: CGPoint,
+        logoTintColor: UIColor = .white,
+        iconPadding: NSDirectionalEdgeInsets
+    ) -> UIButton {
+        let logo = {
+            var logo = UIButton.Configuration.plain()
+            logo.background.image = UIImage(named: logotip)?.withTintColor(logoTintColor)
+            logo.background.backgroundInsets = iconPadding
+            logo.image?.withTintColor(.white, renderingMode: .alwaysOriginal)
+
+            return logo
+        }()
+        let button = UIButton(configuration: logo)
+        button.backgroundColor = color
+        button.frame.size = size
+        button.layer.cornerRadius = corner
+        button.frame.origin = position
+        button.tintColor = .white
+
+        return button
+    }
+}
