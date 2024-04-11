69804a6cf28cbc84238388a27c6882c4467d8559
Martynenko Uriy <uriy.martynenko@gmail.com>
2024-04-11 21:27:50 +0300

    add photo

---
+++ ErrorNil_UIKit_HW5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ErrorNil_UIKit_HW5
+//
+//  Created by Юрий Мартыненко on 11.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
