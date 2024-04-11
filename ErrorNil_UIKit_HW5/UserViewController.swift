388225a634ae9c117832b2aedf1391877dbc3873
Martynenko Uriy <uriy.martynenko@gmail.com>
2024-04-11 23:00:35 +0300

    update

--- ErrorNil_UIKit_HW5/UserViewController.swift
+++ ErrorNil_UIKit_HW5/UserViewController.swift
@@ -7,23 +7,96 @@
 
 import UIKit
 
+
+
 class UserViewController: UIViewController {
+    
+    weak var delegate: ViewControllerDelegate?
+    
+    var userImage: String = ""
+    var fullName: String = ""
+    var userMassage: [String] = []
+    var numSection: Int = 0
+    var numRow: Int = 0
+    var arrayOfDeletedMessages: [String] = []
+    
+    lazy var userImageAvatar: UIImageView = {
+        $0.image = UIImage(named: userImage)
+        $0.frame.size = CGSize(width: 136, height: 136)
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 68
+        $0.frame.origin = CGPoint(x: view.center.x - $0.frame.width / 2, y: 114)
+        
+        
+        return $0
+    }(UIImageView())
+    
+    lazy var userFullNameLabel: UILabel = {
+        $0.text = fullName
+        $0.font = .boldSystemFont(ofSize: 20)
+        $0.frame.size = CGSize(width: view.frame.width - 60, height: 24)
+        $0.frame.origin = CGPoint(x: 30, y: userImageAvatar.frame.maxY + 27)
+        $0.textAlignment = .center
+        
+        return $0
+    }(UILabel())
+    
+    lazy var userCellMessage = createUserMessageCell(userName: fullName, userMassages: userMassage, userImage: userImage)
+    
+    lazy var userMessageTableView: UITableView = {
+        $0.register(UITableViewCell.self, forCellReuseIdentifier: "messageUserCell")
+        $0.dataSource = self
+        $0.delegate = self
+        
+        return $0
+    }(UITableView(frame: CGRect(origin: CGPoint(x: 0, y: userFullNameLabel.frame.maxY + 50), size: CGSize(width: view.frame.width, height: view.frame.height)), style: .insetGrouped))
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        [userImageAvatar, userFullNameLabel, userMessageTableView].forEach {view.addSubview($0)}
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension UserViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        userCellMessage.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "messageUserCell", for: indexPath)
+        let user = userCellMessage[indexPath.row]
+        
+        var config = cell.defaultContentConfiguration()
+        config.text = user.userName
+        config.secondaryText = "\(user.message)"
+        config.image = UIImage(named: user.image)
+        config.imageProperties.maximumSize = CGSize(width: 40, height: 40)
+        config.imageProperties.cornerRadius = 20
+        
+        cell.contentConfiguration = config
+        
+        return cell
+    }
+}
+
+
+extension UserViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//        print(indexPath)
+//        print(userCellMessage[indexPath.row].message)
+        arrayOfDeletedMessages.append(userCellMessage[indexPath.row].message)
+        userCellMessage.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+        
+        
+//        print(arrayOfDeletedMessages)
+        
+        self.delegate?.deletingUserMessage(section: numSection, row: numRow, messages: arrayOfDeletedMessages)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        "MESSAGES"
     }
-    */
-
 }
--- ErrorNil_UIKit_HW5/ViewController.swift
+++ ErrorNil_UIKit_HW5/ViewController.swift
@@ -7,13 +7,109 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+protocol ViewControllerDelegate: AnyObject {
+    func deletingUserMessage(section: Int, row: Int, messages: [String])
 }
 
+
+class ViewController: UIViewController {
+    private var users = generatorCellUser()
+    
+    lazy var tableView: UITableView = {
+        $0.register(UITableViewCell.self, forCellReuseIdentifier: "userCell")
+        $0.dataSource = self
+        $0.layer.cornerRadius = 30
+        $0.delegate = self
+        
+        return $0
+    }(UITableView(frame: view.frame, style: .insetGrouped))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        
+        title = "Friends"
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+}
+
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        users.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        users[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath)
+        let user = users[indexPath.section][indexPath.row]
+        
+        var config = cell.defaultContentConfiguration()
+        config.text = user.userName
+        config.secondaryText = "\(user.messages.count) сообщений"
+        config.image = UIImage(named: user.image)
+        config.imageProperties.maximumSize = CGSize(width: 40, height: 40)
+        config.imageProperties.cornerRadius = 20
+        
+        cell.contentConfiguration = config
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
+    }
+}
+
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //        print(users[indexPath.section][indexPath.row].id, users[indexPath.section][indexPath.row].userName, users[indexPath.section][indexPath.row].messages)
+        let userVC = UserViewController()
+        userVC.fullName = users[indexPath.section][indexPath.row].userName
+        userVC.userImage = users[indexPath.section][indexPath.row].image
+        userVC.userMassage = users[indexPath.section][indexPath.row].messages
+        userVC.numSection = indexPath.section
+        userVC.delegate = self
+        userVC.numRow = indexPath.row
+        self.navigationController?.pushViewController(userVC, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        print(users[indexPath.section][indexPath.row].messages)
+        users[indexPath.section][indexPath.row].messages.remove(at: 0)
+        print(users[indexPath.section][indexPath.row].messages)
+        print(users[1][0].messages)
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+            case 0:
+                return "NEW"
+            case 1:
+                return "BEST"
+            default:
+                return ""
+        }
+    }
+}
+
+extension ViewController: ViewControllerDelegate {
+    func deletingUserMessage(section: Int, row: Int, messages: [String]) {
+        for message in messages {
+            for (index, messageDelete) in users[section][row].messages.enumerated() {
+                if messageDelete == message {
+                    users[section][row].messages.remove(at: index)
+                }
+            }
+        }
+        tableView.reloadData()
+    }
+}
