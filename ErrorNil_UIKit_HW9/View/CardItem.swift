9ec86748eb423451d0e9c1f25fd091b9e5cfef4d
Martynenko Uriy <uriy.martynenko@gmail.com>
2024-04-26 08:19:47 +0300

    added a gray cell

--- ErrorNil_UIKit_HW9/View/CardItem.swift
+++ ErrorNil_UIKit_HW9/View/CardItem.swift
@@ -28,6 +28,13 @@ class CardItem: UICollectionViewCell, CellProtocol {
         fatalError("init(coder:) has not been implemented")
     }
     
+    lazy var cellView: UIView = {
+        .config(view: $0) { setupCell in
+            setupCell.backgroundColor = .appCellGray
+            setupCell.layer.cornerRadius = 30
+        }
+    }(UIView())
+    
     lazy var userImageAvatar = AppElements.createImage(scale: .scaleAspectFill, corner: 22)
     lazy var userName = AppElements.createLabel(
         font: .systemFont(ofSize: 20, weight: .bold),
@@ -53,7 +60,9 @@ class CardItem: UICollectionViewCell, CellProtocol {
     lazy var textTweat = AppElements.createLabel(font: .systemFont(ofSize: 16), textColor: .black, aligment: .left)
     
     func setupCell(item: Item) {
-        [userImageAvatar, userName, imageView, postCreationDate, title, textTweat].forEach {addSubview($0)}
+//        [userImageAvatar, userName, imageView, postCreationDate, title, textTweat].forEach {addSubview($0)}
+        [userImageAvatar, userName, imageView, postCreationDate, title, textTweat].forEach {cellView.addSubview($0)}
+        addSubview(cellView)
         
         userImageAvatar.image = UIImage(named: item.userAvatar)
         userName.text = item.userName
@@ -74,33 +83,39 @@ class CardItem: UICollectionViewCell, CellProtocol {
         }
 
         NSLayoutConstraint.activate([
-            userImageAvatar.topAnchor.constraint(equalTo: topAnchor),
-            userImageAvatar.leadingAnchor.constraint(equalTo: leadingAnchor),
+            cellView.topAnchor.constraint(equalTo: topAnchor),
+            cellView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            cellView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            cellView.widthAnchor.constraint(equalToConstant: viewWidth - 60),
+            textTweat.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+            
+            userImageAvatar.topAnchor.constraint(equalTo: cellView.topAnchor, constant: 20),
+            userImageAvatar.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: 20),
             userImageAvatar.widthAnchor.constraint(equalToConstant: 44),
             userImageAvatar.heightAnchor.constraint(equalToConstant: 44),
             
             userName.leadingAnchor.constraint(equalTo: userImageAvatar.trailingAnchor, constant: 20),
             userName.centerYAnchor.constraint(equalTo: userImageAvatar.centerYAnchor),
-            userName.trailingAnchor.constraint(equalTo: trailingAnchor),
+            userName.trailingAnchor.constraint(equalTo: cellView.trailingAnchor, constant: -20),
             
             imageView.topAnchor.constraint(equalTo: userImageAvatar.bottomAnchor, constant: 20),
-            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            imageView.widthAnchor.constraint(equalToConstant: viewWidth - 60),
+            imageView.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: 20),
+            imageView.trailingAnchor.constraint(equalTo: cellView.trailingAnchor, constant: -20),
+//            imageView.widthAnchor.constraint(equalToConstant: viewWidth - 60),
             imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: imageRatio),
             
             postCreationDate.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 9),
-            postCreationDate.leadingAnchor.constraint(equalTo: leadingAnchor),
-            postCreationDate.trailingAnchor.constraint(equalTo: trailingAnchor),
+            postCreationDate.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: 20),
+            postCreationDate.trailingAnchor.constraint(equalTo: cellView.trailingAnchor, constant: -20),
             
             title.topAnchor.constraint(equalTo: postCreationDate.bottomAnchor, constant: 4),
-            title.leadingAnchor.constraint(equalTo: leadingAnchor),
-            title.trailingAnchor.constraint(equalTo: trailingAnchor),
+            title.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: 20),
+            title.trailingAnchor.constraint(equalTo: cellView.trailingAnchor, constant: -20),
             
             textTweat.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 10),
-            textTweat.leadingAnchor.constraint(equalTo: leadingAnchor),
-            textTweat.trailingAnchor.constraint(equalTo: trailingAnchor),
-            textTweat.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20)
+            textTweat.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: 20),
+            textTweat.trailingAnchor.constraint(equalTo: cellView.trailingAnchor, constant: -20),
+            textTweat.bottomAnchor.constraint(equalTo: cellView.bottomAnchor, constant: -20)
         ])
     }
 }
