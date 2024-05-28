4762986b89cfd694b80c82e1b5a589c009eeb5d1
Martynenko Uriy <uriy.martynenko@gmail.com>
2024-05-28 06:21:27 +0300

    update

---
+++ Carousel/NewViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NewViewController.swift
+//  Carousel
+//
+//  Created by Юрий Мартыненко on 21.05.2024.
+//
+
+import UIKit
+
+class NewViewController: ViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Carousel/SceneDelegate.swift
+++ Carousel/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         self.window = UIWindow(windowScene: scene)
-        self.window?.rootViewController = ViewController()
+        self.window?.rootViewController = NewViewController()
         self.window?.makeKeyAndVisible()
         
     }
